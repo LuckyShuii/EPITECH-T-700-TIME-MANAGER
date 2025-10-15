0cbd65f802e4ba1dccdb0be5d404712efa690375
LuckyShuii <lucasnimes30000@gmail.com>
2025-10-15 16:27:30 +0200

    (ops) added archive logs scripts

--- scripts/init_log_files.sh
+++ scripts/init_log_files.sh
@@ -4,6 +4,12 @@ PWD=$(pwd)
 
 # Create logs directory if it doesn't exist
 mkdir -p "$PWD/../logs"
+mkdir -p "$PWD/../logs/archives"
+
+mkdir -p "$PWD/../logs/archive/frontend"
+mkdir -p "$PWD/../logs/archive/backend"
+mkdir -p "$PWD/../logs/archive/database"
+mkdir -p "$PWD/../logs/archive/apigateway"
 
 # Create empty log files if they don't exist
 touch "$PWD/../logs/frontend.log"
